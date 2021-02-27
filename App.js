684ce3c16d37953d1530bb7aa421628c6671b26e
Chris Paterson <chrispaterson22@gmail.com>
2021-02-27 14:42:32 +0000

    stylesheets and state, scrollview list items

--- App.js
+++ App.js
@@ -1,33 +1,71 @@
 import { StatusBar } from 'expo-status-bar'
-import React from 'react'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
+import {
+	StyleSheet,
+	Text,
+	View,
+	TextInput,
+	Button,
+	ScrollView,
+} from 'react-native'
 
 export default function App() {
+	const [enteredGoal, setEnteredGoal] = useState('')
+	const [courseGoals, setCourseGoals] = useState([])
+
+	const goalInputHandler = enteredText => {
+		setEnteredGoal(enteredText)
+	}
+
+	const addGoalHandler = () => {
+		setCourseGoals(currentGoals => [...courseGoals, enteredGoal]) //using arr function ensures current courseGoals is retrieved.
+	}
+
 	return (
-		<View style={{ paddingTop: 60, paddingHorizontal: 30 }}>
-			<View
-				style={{
-					flexDirection: 'row',
-					justifyContent: 'space-between',
-					alignItems: 'center',
-				}}
-			>
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
 				<TextInput
 					placeholder='Course Goal'
-					style={{
-						width: '80%',
-						height: 40,
-						borderColor: 'black',
-						borderWidth: 1,
-						padding: 10,
-					}}
+					style={styles.input}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
 				/>
-				<Button title='ADD' />
+				<Button title='ADD' onPress={addGoalHandler} />
 			</View>
-			<View></View>
+			<ScrollView>
+				{courseGoals.map(goal => (
+					<View key={goal} style={styles.listItem}>
+						<Text>{goal}</Text>
+					</View> //shouldn't use goal as key as can enter same goal more than once and throw error!
+				))}
+			</ScrollView>
 			<StatusBar style='auto' />
 		</View>
 	)
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+	screen: {
+		paddingTop: 60,
+		paddingHorizontal: 30,
+	},
+	inputContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	input: {
+		width: '80%',
+		height: 40,
+		borderColor: 'black',
+		borderWidth: 1,
+		padding: 10,
+	},
+	listItem: {
+		padding: 10,
+		marginVertical: 10,
+		backgroundColor: '#ccc',
+		borderColor: 'black',
+		borderWidth: 1,
+	},
+})
